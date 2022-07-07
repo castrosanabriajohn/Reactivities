ec1b5dda54789d82d9e17ab63499df588d004ad3
jcastro97 <jcastro@crnova.com>
2022-07-06 20:40:48 -0600

    Add NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useState} from 'react';
-import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then(response => {
-      setActivities(response.data);
-    })
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities'/>
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
         <List>
-          {activities.map((activity: any) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
-    </div>
+      </Container>
+      {/* <Header as="h2" icon="users" content="Reactivities" /> */}
+    </>
   );
 }
 
