2b96a53f2e53d7947fcd4f54d5374193a92770f7
jcastro97 <jcastro@crnova.com>
2022-07-12 16:04:14 -0600

    Styling

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,48 +1,34 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react-lite";
-import { Button, Card, Image } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import DetailsHeader from "./DetailsHeader";
+import DetailsInfo from "./DetailsInfo";
+import DetailsChat from "./DetailsChat";
+import DetailsSidebar from "./DetailsSidebar";
 
 const ActivityDetails = () => {
   const { activityStore } = useStore();
-  const { currentActivity, loadSingleActivity, initialLoadingState } = activityStore;
+  const { currentActivity, loadSingleActivity, initialLoadingState } =
+    activityStore;
   const { id } = useParams<{ id: string }>();
   useEffect(() => {
     if (id) loadSingleActivity(id);
-  }, [id, loadSingleActivity])
+  }, [id, loadSingleActivity]);
   if (initialLoadingState || !currentActivity) return <LoadingComponent />;
   return (
-    <Card fluid>
-      <Image src={`/assets/categoryImages/${currentActivity.category}.jpg`} />
-      <Card.Content>
-        <Card.Header>{currentActivity.title}</Card.Header>
-        <Card.Meta>
-          <span>{currentActivity.date}</span>
-        </Card.Meta>
-        <Card.Description>{currentActivity.description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button.Group widths="2">
-          <Button
-            as={Link}
-            to={`/manage/${currentActivity.id}`}
-            basic
-            color="blue"
-            content="Edit"
-          />
-          <Button
-            as={Link}
-            to="/activities"
-            basic
-            color="grey"
-            content="Cancel"
-            onClick={() => null}
-          />
-        </Button.Group>
-      </Card.Content>
-    </Card>
+    <Grid>
+      <Grid.Column width={10}>
+        <DetailsHeader activity={currentActivity} />
+        <DetailsInfo activity={currentActivity} />
+        <DetailsChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <DetailsSidebar />
+      </Grid.Column>
+    </Grid>
   );
 };
 export default observer(ActivityDetails);
