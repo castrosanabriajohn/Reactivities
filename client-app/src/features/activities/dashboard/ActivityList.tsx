e9210a497542c208fe20b893945efd418945838c
jcastro97 <jcastro@crnova.com>
2022-07-12 14:51:13 -0600

    Styling List Items

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { observer } from "mobx-react-lite";
-import { Header, Item, Segment } from "semantic-ui-react";
+import { Header, Item } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import ActivityListItem from "./ActivityListItem";
 
@@ -15,13 +15,11 @@ const ActivityList = () => {
           <Header sub color="teal">
             {group}
           </Header>
-          <Segment>
             <Item.Group>
               {array.map((activity) => (
                 <ActivityListItem key={activity.id} activity={activity} />
               ))}
             </Item.Group>
-          </Segment>
         </Fragment>
       ))}
     </>
--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import React, { SyntheticEvent, useState } from "react";
 import { observer } from "mobx-react-lite";
-import { Button, Item, Label } from "semantic-ui-react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
 import { useStore } from "../../../app/stores/store";
@@ -19,36 +19,40 @@ const ActivityListItem = ({ activity }: Props) => {
   }
 
   return (
-    <Item key={activity.id}>
-      <Item.Content>
-        <Item.Header as="a">{activity.title}</Item.Header>
-        <Item.Meta>{activity.date}</Item.Meta>
-        <Item.Description>
-          <div>{activity.description}</div>
-          <div>
-            {activity.city}, {activity.venue}
-          </div>
-        </Item.Description>
-        <Item.Extra>
-          <Button
-            as={Link}
-            to={`/activities/${activity.id}`}
-            floated="right"
-            content="View"
-            color="blue"
-          />
-          <Button
-            name={activity.id}
-            floated="right"
-            content="Delete"
-            color="red"
-            loading={isLoadingFlag && target === activity.id}
-            onClick={(e) => handleDeletion(e, activity.id)}
-          />
-          <Label basic content={activity.category} />
-        </Item.Extra>
-      </Item.Content>
-    </Item>
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image size="tiny" circular src="/assets/user.png" />
+            <Item.Content>
+              <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                {activity.title}
+              </Item.Header>
+              <Item.Description>Hosted by username</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name="clock" />
+          {activity.date}
+          <Icon name="marker" />
+          {activity.venue}
+        </span>
+      </Segment>
+      <Segment secondary>Attendees</Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          color="teal"
+          floated="right"
+          content="View"
+        />
+      </Segment>
+    </Segment.Group>
   );
 };
 export default observer(ActivityListItem);
