ea52bd7d73b4d6a39e8e5459e3b860c9e58c069b
jcastro97 <jcastro@crnova.com>
2022-07-15 17:57:15 -0600

    Set Token upon login

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
 import { User, UserFormValues } from "../models/user";
 import agent from "../api/agent";
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import { store } from "./store";
+import { history } from "../..";
 
 export default class UserStore {
   user: User | null = null;
@@ -16,9 +18,19 @@ export default class UserStore {
   login = async (creds: UserFormValues) => {
     try {
       const user = await agent.accountRequests.login(creds);
+      store.commonStore.setToken(user.token); // after login receive and store the token
+      runInAction(() => (this.user = user)); // set the user
+      history.push("/activities");
       console.log(user);
     } catch (e) {
       throw e; // Throwing the error to the component will make it avaible to bwe catched by the onSubmit on the form
     }
+  };
+
+  logout = () => {
+    store.commonStore.setToken(null);
+    window.localStorage.removeItem("jwt");
+    runInAction(() => (this.user = null));
+    history.push("/");
   };
 }
