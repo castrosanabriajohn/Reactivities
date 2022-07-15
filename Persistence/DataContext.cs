f608ed7aadedcca119285b8db8fb7daa1ccbf520
jcastro97 <jcastro@crnova.com>
2022-07-15 10:08:15 -0600

    Identity Migration added

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
     // Is going to be used as a service that allows injecting the data context into other classes in the application
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
