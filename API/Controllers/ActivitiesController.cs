f30c78433929f7135ca57d6447976703f4aea1e9
jcastro97 <jcastro@crnova.com>
2022-07-17 09:42:41 -0600

    Testing Create with Host

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,11 +1,9 @@
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Application.Activities;
-using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-  [AllowAnonymous]
   public class ActivitiesController : BaseApiController
   {
     [HttpGet]
